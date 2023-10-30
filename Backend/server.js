7142c81d20e4804b25e34d47f763aa9fe89faedb
AlmogHasson <almoghasson12345@gmail.com>
2023-10-30 19:11:05 +0200

    added origin

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(bodyParder.json());
 app.use(cors({
   // origin: 'http://localhost:3001',
-  origin:"https://almoghasson.github.io/Face-Recognition/",
+  origin:["https://almoghasson.github.io/Face-Recognition/","https://almoghasson.github.io/"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   
